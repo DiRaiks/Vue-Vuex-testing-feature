7073daf2c4a0e5de0375288d1afaca7bfc8f92bd
DiRaiks <t.fane@mail.ru>
2018-05-29 23:30:17 +0300

    changed routing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
+  <div>
     <h1> Hello World! </h1>
     <div class="">
       <router-link to="/">Home</router-link> |
-      <!-- <router-link to="/registration">Registration</router-link> -->
+      <router-link to="/registration">Registration</router-link>
     </div>
     <router-view/>
   </div>
--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router/routes.js'
+import router from './router'
 
 new Vue({
   router,
